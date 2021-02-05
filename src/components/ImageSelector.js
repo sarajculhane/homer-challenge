b9ffead98206a96fd23e5e7a2c01cd33a4c89399
Sara Culhane <sjcully@mac.com>
2021-02-05 15:08:40 -0500

    select items

--- src/components/ImageSelector.js
+++ src/components/ImageSelector.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {SmallImage} from './index'
 
 const ImageSelector = (props) => {
+
+    // get data from props and sort alphabetically
     let data = props.data.carouselImages
     data.sort((a, b) => a.imageCaption.toLowerCase().trim() > b.imageCaption.toLowerCase().trim() ? 1 : -1)
-    console.log(data)
+
+    const [selectedImages, setSelectedImages] = useState([])
+    const [selected, setSelected] = useState(false)
+
+    const handleClick = (e) => {
+        // setValue({value: e.target.value})
+        e.preventDefault()
+        setSelected(!selected)
+        let eventName = `${e.target.name}`
+        if(!selectedImages.includes(eventName)) setSelectedImages(prev => [...prev, eventName])
+        else setSelectedImages(prev => prev.filter((id) => id === eventName))
+
+        console.log(event.target, e.target.name, selectedImages)
+    }
+
+    const handleAdd = () => {
+        console.log('added', selectedImages)
+    }
+
     return (
         <div className='img-container'>
-                {data.map((img) => <div><SmallImage img={img} /></div>)}
+                {data.map((img, id) => <div><SmallImage img={img} handleClick={handleClick} id={id+1} selectedImages={selectedImages} /></div>)}
+             { selectedImages.length ?   <button className='btn' onClick={handleAdd}>Add</button> : <button className='btn' disabled>Add</button>}
         </div>
     )
 }
 
-export default ImageSelector;                              
+export default ImageSelector;                         
--- src/components/SmallImage.js
+++ src/components/SmallImage.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React ,{useState} from 'react'
 const SmallImage = (props) => {
-    const {img} = props
+    const {img, handleClick, id, selected, selectedImages} = props
+    console.log(selectedImages)
+    const [select, setSelect] = useState([])
+
+    const testClick = () => {
+        if(!select.includes(id)) setSelect(prev => [...prev, id])
+        else setSelect(select.filter((imgId) => imgId !== id))
+    }
+    console.log(selectedImages.filter((image) => image.eventName === id))
     return (
-        <div>
-            <div><img src={`/images/${img.imageName}`} className='small' /></div>
+        <div onClick={handleClick} >
+            <div onClick={testClick}><img src={`/images/${img.imageName}`} className={`small ${ select.includes(id) ? 'selected' : ''}`} name={id} /></div>
             <div>{img.imageCaption}</div>
         </div>
     )
